95fc7700ddc20afab0b3f7c586c954c95f491563
Sisi Wang <sisiwang242@gmail.com>
2024-03-03 09:02:55 -0500

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -25,36 +25,27 @@ function App() {
       setShowQuestion(false);
     
   }
+
   return (
     <>
     <div className='w-full min-h-screen bg-[url("./assets/flower1.jpg")] bg-cover flex flex-col justify-center items-center text-xl text-center tracking-wide'>
       <div className='grid lg:grid-cols-2 sm:grid-cols-1 leading-loose '>
-        <div className='pl-4'>
+        <div className='pl-4 pr-4'>
           <h2 className='text-4xl pb-6'>The Ultimate Plant Parent!</h2>
           <p>How good of a plant parent are you? Test all of your plant knowledge here!</p>
-          <p>Number of cards: 10</p>
+          <p>Number of cards: {cardId}</p>
 
-          <div onClick={handleShowAnswer} 
-            className={`${showAnswer ? 'pointer-events-none' : ''} border border-gray-700 shadow-sm shadow-gray-500 w-[700px] pl-4 pr-4 h-[200px] flex justify-center items-center mt-8 rounded-md animate-pulse`} 
-            style={{backgroundColor: `${plantData[cardId].color}`}}
-          >
-            {showQuestion && <h2>{plantData[cardId].Question}</h2>}
-            {showAnswer && <h2>{plantData[cardId].Answer}</h2>}
+          <div onClick={handleShowAnswer} className={`${showAnswer ? 'pointer-events-none' : ''} border border-gray-700 shadow-sm shadow-gray-500 lg:w-[700px] sm:w-auto h-[200px] flex justify-center items-center mt-8 rounded-md hover:scale-110`} 
+            style={{backgroundColor: plantData[cardId]?.color || 'white'}}>
+            {showQuestion && <h2>{plantData[cardId]?.Question}</h2>}
+            {showAnswer && <h2>{plantData[cardId]?.Answer}</h2>}
           </div>
 
           <div className='flex gap-8 justify-center pt-10'>
-            <button 
-              type="button" 
-              className='text-white bg-rose-300 w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6] hover:bg-[#f43f5e] active:bg-[#e11d48] focus:outline-none focus:ring focus:ring-[#fecdd3]'
-              onClick={handlePreviousBtn}
-            >
+            <button type="button" onClick={handlePreviousBtn} className='text-white bg-rose-300 w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6] hover:bg-[#f43f5e] active:bg-[#e11d48] focus:outline-none focus:ring focus:ring-[#fecdd3]'>
               &larr;
             </button>
-            <button
-              type="button" 
-              className='text-white bg-rose-300 w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6] hover:bg-[#f43f5e] active:bg-[#e11d48] focus:outline-none focus:ring focus:ring-[#fecdd3]'
-              onClick={handleNextBtn}
-            >
+            <button type="button" onClick={handleNextBtn} className='text-white bg-rose-300 w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6] hover:bg-[#f43f5e] active:bg-[#e11d48] focus:outline-none focus:ring focus:ring-[#fecdd3]' >
               &#8594;
             </button>
           </div>
