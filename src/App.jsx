f321b60b49667140cc725a0c3b7441d3e9f7cfcf
Sisi Wang <sisiwang242@gmail.com>
2024-03-02 21:43:57 -0500

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
 import { useState } from 'react'
+import plantData from "./component/Data.jsx";
 
 function App() {
-  const [data, setData] = useState({data})
+  const [cardId, setCardId] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [showQuestion, setShowQuestion] = useState(true);
+
+  const handlePreviousBtn = () => {
+    if (cardId > 0) {
+      setCardId(cardId - 1);
+      setShowAnswer(false);
+      setShowQuestion(true);
+    }
+  }
+  const handleNextBtn = () => {
+    if (cardId < plantData.length - 1) {
+      setCardId(cardId + 1);
+      setShowAnswer(false);
+      setShowQuestion(true);
+    }
+  }
+  const handleShowAnswer = () => {
+      setShowAnswer(true);
+      setShowQuestion(false);
+    
+  }
   return (
     <>
     <div className='w-full min-h-screen bg-[url("./assets/flower1.jpg")] bg-cover flex flex-col justify-center items-center text-xl text-center tracking-wide'>
       <div className='grid lg:grid-cols-2 sm:grid-cols-1 leading-loose '>
         <div className='pl-4'>
-          <h2 className='text-4xl'>The Ultimate Plant Parent!</h2>
+          <h2 className='text-4xl pb-6'>The Ultimate Plant Parent!</h2>
           <p>How good of a plant parent are you? Test all of your plant knowledge here!</p>
           <p>Number of cards: 10</p>
-          <div className='flex gap-8 justify-center pt-8'>
+
+          <div onClick={handleShowAnswer} 
+            className={`${showAnswer ? 'pointer-events-none' : ''} border border-gray-700 shadow-sm shadow-gray-500 w-[700px] pl-4 pr-4 h-[200px] flex justify-center items-center mt-8 rounded-md animate-pulse`} 
+            style={{backgroundColor: `${plantData[cardId].color}`}}
+          >
+            {showQuestion && <h2>{plantData[cardId].Question}</h2>}
+            {showAnswer && <h2>{plantData[cardId].Answer}</h2>}
+          </div>
+
+          <div className='flex gap-8 justify-center pt-10'>
             <button 
               type="button" 
-              className='text-white bg-[#fda4af] w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6]'
+              className='text-white bg-rose-300 w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6] hover:bg-[#f43f5e] active:bg-[#e11d48] focus:outline-none focus:ring focus:ring-[#fecdd3]'
+              onClick={handlePreviousBtn}
             >
               &larr;
             </button>
             <button
               type="button" 
-              className='text-white bg-[#fda4af] w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6]'
+              className='text-white bg-rose-300 w-[70px] text-3xl rounded-full shadow-md shadow-[#ffe4e6] hover:bg-[#f43f5e] active:bg-[#e11d48] focus:outline-none focus:ring focus:ring-[#fecdd3]'
+              onClick={handleNextBtn}
             >
               &#8594;
             </button>
